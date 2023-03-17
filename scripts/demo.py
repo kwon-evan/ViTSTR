619257b202c0e69194e12e4cf213e1f9a7b5bab0
kwon-evan <koun012@naver.com>
2023-03-17 16:55:03 +0900

    open image each

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,9 +1,11 @@
+import os
 import yaml
 import torch
 from argparse import Namespace
 from vitstr import ViTSTR
 from PIL import Image
 from glob import glob
+from rich.progress import track
 
 if __name__ == "__main__":
     """load configuration"""
@@ -11,9 +13,20 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     """load model"""
-    model = ViTSTR.load_from(opt.saved_model).to(device)
+    model = ViTSTR.load_from(opt.saved_model).to(device).eval()
 
     """inference"""
-    images = [Image.open(path) for path in glob("demo_images/*.jpg")]
-    for image in images:
-        print(model.imread(image))
+    paths = glob("img_plate/*.jpg")
+
+    for path in track(paths):
+        image = Image.open(path).convert("RGB")
+        pred, conf = model.imread(image)
+
+        # if img exists, add extra numbering to pred
+        if os.path.exists(f"img_plate_read/{pred}.jpg"):
+            i = 1
+            while os.path.exists(f"img_plate_read/{pred}-{i}.jpg"):
+                i += 1
+            pred = f"{pred}_{i}"
+
+        image.save(f"img_plate_read/{pred}.jpg", "JPEG")
